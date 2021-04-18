fbbb6dc3844848fdb2fd76e251481aa7afe6244d
Muhammad Anas Imtiaz <anasimtiaz200899@gmail.com>
2021-04-18 13:17:46 +0500

    Setup API Error Handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,7 +5,9 @@ using API.Core.Specifications;
 using API.Data;
 using API.Dtos;
 using API.Entities;
+using API.Errors;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +15,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
 
         private readonly IGenericRepository<ProductBrand> _brandRepo;
@@ -41,11 +43,15 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecifications(id);
 
             var product = await _productRepo.GetEntityWithSpec(spec);
+            
+            if( product == null )return NotFound(new ApiResponse(404));
 
             return _mapper.Map<Product,ProductToReturnDto>(product);
 
